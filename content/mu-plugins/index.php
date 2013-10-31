89abcb91837372a7ce36b0beed0e6dbe2653a346
jeremyfelt <jeremy.felt@gmail.com>
2013-10-31 12:17:31 -0700

    Add a header to the blank index file in mu-plugins for nicer looking admin

--- content/mu-plugins/index.php
+++ content/mu-plugins/index.php
@@ -1,2 +1,10 @@
 <?php
-// Blank files make for blank requests.
+/*
+ * Plugin Name: Bland Index File
+ * Plugin URI: http://web.wsu.edu
+ * Description: This is a blank index file with an unnecessary description.
+ * Author: washingtonstateuniversity, jeremyfelt
+ * Author URI: http://web.wsu.edu
+ * Version: 0.1
+ * Network: true
+ */
