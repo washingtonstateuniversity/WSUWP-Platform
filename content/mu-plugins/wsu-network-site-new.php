2b60e54f4ec223a83b2b69f17bc0b9846efd36d5
jeremyfelt <jeremy.felt@gmail.com>
2013-11-15 16:11:38 -0800

    Implement our version of the add-blog process from site-new.php
    This allows for both a site domain and path to be submitted for a network.

--- content/mu-plugins/wsu-network-site-new.php
+++ content/mu-plugins/wsu-network-site-new.php
@@ -10,8 +10,108 @@
 * Network: true
 */
 
-function wsu_create_new_site( $site ) {
-	// Create a site with posted data.
+function wsu_create_new_site( $site  ) {
+	global $wpdb;
+
+	if ( empty( $site['domain'] ) || empty( $site['path'] ) ) {
+		wp_die( __( 'Missing site domain or path.' ) );
+	}
+
+	if ( empty( $site['email'] ) ) {
+		wp_die( __( 'Missing site administrator email.' ) );
+	}
+
+	if ( empty( $site['title'] ) ) {
+		wp_die( __( 'Missing site title.' ) );
+	}
+
+	$email = sanitize_email( $site['email'] );
+
+	if ( ! is_email( $email ) ) {
+		wp_die( __( 'Invalid email address.' ) );
+	}
+
+	// These use the standard preg_match from WordPress core as of 3.7.1
+	// @todo revisit this standard
+	$domain = '';
+	if ( preg_match( '|^([a-zA-Z0-9-])+$|', $site['domain'] ) ) {
+		$domain = strtolower( $site['domain'] );
+	}
+
+	$path = '';
+	if ( preg_match( '|^([a-zA-Z0-9-])+$|', $site['path'] ) ) {
+		$path = strtolower( $site['path'] );
+	}
+
+	// Once the preg_match has been applied, we should error if any changes were made.
+	if ( $domain !== $site['domain'] ) {
+		wp_die( __( 'Invalid site domain.' ) );
+	}
+
+	if ( $path !== $site['path'] ) {
+		wp_die( __( 'Invalid site path.' ) );
+	}
+
+	/**
+	 * A general note that I'm removing some checks for subdomain installs as I
+	 * don't yet believe the separation will be necessary for our use case. Once
+	 * this is confirmed, we can go back through and add the checks as appropriate.
+	 *
+	 * It is very possible that the new checks can be filtered so that individual
+	 * networks can determine what is supported.
+	 */
+
+	$subdirectory_reserved_names = apply_filters( 'subdirectory_reserved_names', array( 'page', 'comments', 'blog', 'files', 'feed' ) );
+	if ( in_array( $site['path'], $subdirectory_reserved_names ) ) {
+		wp_die( sprintf( __('The following words are reserved for use by WordPress functions and cannot be used as blog names: <code>%s</code>' ), implode( '</code>, <code>', $subdirectory_reserved_names ) ) );
+	}
+
+	// Build the desired domain
+	if ( '' === $domain ) {
+		$new_domain = get_current_site()->domain;
+	} else {
+		$new_domain = $domain . '.' . preg_replace( '|^www\.|', '', get_current_site()->domain );
+	}
+
+	if ( '' === $path ) {
+		$new_path = get_current_site()->path;
+	} else {
+		$new_path = get_current_site()->path . '/' . $path . '/';
+	}
+
+	$password = 'N/A';
+	$user_id = email_exists( $email );
+
+	if ( ! $user_id ) { // Create a new user with a random password
+		$password = wp_generate_password( 12, false );
+		$user_id = wpmu_create_user( $domain, $password, $email );
+		if ( false == $user_id )
+			wp_die( __( 'There was an error creating the user.' ) );
+		else
+			wp_new_user_notification( $user_id, $password );
+	}
+
+	$wpdb->hide_errors();
+	$id = wpmu_create_blog( $new_domain, $new_path, $site['title'], $user_id , array( 'public' => 1 ), get_current_site()->id );
+	$wpdb->show_errors();
+
+	if ( is_wp_error( $id ) ) {
+		wp_die( $id->get_error_message() );
+	}
+
+	if ( ! is_super_admin( $user_id ) && ! get_user_option( 'primary_blog', $user_id ) ) {
+		update_user_option( $user_id, 'primary_blog', $id, true );
+	}
+
+	$content_mail = sprintf( __( 'New site created by %1$s
+
+Address: %2$s
+Name: %3$s' ), wp_get_current_user()->user_login , get_site_url( $id ), wp_unslash( $site['title'] ) );
+	wp_mail( get_site_option('admin_email'), sprintf( __( '[%s] New Site Created' ), get_current_site()->site_name ), $content_mail, 'From: "Site Admin" <' . get_site_option( 'admin_email' ) . '>' );
+	wpmu_welcome_notification( $id, $user_id, $password, $site['title'], array( 'public' => 1 ) );
+	wp_redirect( add_query_arg( array( 'update' => 'added', 'id' => $id ), 'site-new.php' ) );
+
+	exit;
 }
 
 add_action( 'network_admin_notices', 'wsu_new_site' );
