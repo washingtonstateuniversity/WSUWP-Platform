da27ea70f622c1413c4ecd441e4d66f619b60e89
jeremyfelt <jeremy.felt@gmail.com>
2013-11-18 12:11:03 -0800

    Introduce a WSUWP install.php drop-in

---
+++ content/install.php
@@ -0,0 +1,9 @@
+<?php
+/*
+ * Plugin Name: WSUWP Install Drop-In
+ * Plugin URI: http://web.wsu.edu
+ * Description: install.php drop-in to provide replacement functions for various install and upgrade features.
+ * Author: washingtonstateuniversity, jeremyfelt
+ * Author URI: http://web.wsu.edu
+ * Version: 0.1
+ */
