70fece5e3414ad1d3eb57dc47d74957d70e8544f
jeremyfelt <jeremy.felt@gmail.com>
2014-03-19 11:28:03 -0700

    Remove unused WP-CLI constant from sunrise

--- www/wp-content/sunrise.php
+++ www/wp-content/sunrise.php
@@ -37,11 +37,6 @@
  * @type WPDB $wpdb
  */
 
-// If wp-cli is in use, then avoid sunrise.
-if ( isset( $_SERVER['WP_CLI_PHP_USED'] ) ) {
-	return;
-}
-
 // Remove strict standards reporting, only show notices and warnings.
 if ( WP_DEBUG && WSU_DISABLE_STRICT )
 	error_reporting( E_ALL ^ E_STRICT );
