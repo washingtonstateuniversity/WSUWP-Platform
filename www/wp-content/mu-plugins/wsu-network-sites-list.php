f053d2771e9501baae3a54f2ec50e2175091e788
jeremyfelt <jeremy.felt@gmail.com>
2014-04-01 11:40:11 -0700

    Assign an empty string to spam/unspam rather than unsetting
    This seems more appropriate, as defaults of empty strings are set
    in the original array.

--- www/wp-content/mu-plugins/wsu-network-sites-list.php
+++ www/wp-content/mu-plugins/wsu-network-sites-list.php
@@ -128,10 +128,10 @@ class WSU_Network_Sites_List {
 	 */
 	public function manage_sites_action_links( $actions, $site_id, $site_name ) {
 		if ( isset( $actions['spam'] ) ) {
-			unset( $actions['spam'] );
+			$actions['spam'] = '';
 		}
 		if ( isset( $actions['unspam'] ) ) {
-			unset( $actions['unspam'] );
+			$actions['unspam'] = '';
 		}
 
 		return $actions;
