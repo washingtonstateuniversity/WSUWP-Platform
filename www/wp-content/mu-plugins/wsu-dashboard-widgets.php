40cbbdb98c39d80ec1269bf1b5849480c2c64158
jeremyfelt <jeremy.felt@gmail.com>
2014-04-01 14:09:05 -0700

    Move dashboard removal functions into a class

--- www/wp-content/mu-plugins/wsu-dashboard-widgets.php
+++ www/wp-content/mu-plugins/wsu-dashboard-widgets.php
@@ -1,34 +1,47 @@
 <?php
 /*
-Plugin Name: WSU Remove Dashboard Widgets
+Plugin Name: WSUWP Dashboard
 Plugin URI: http://web.wsu.edu/
-Description: Removes parts of the WordPress dashboard that WSU does not need.
+Description: Modifications to the WordPress Dashboard.
 Author: washingtonstateuniversity, jeremyfelt
 Version: 0.1
 */
 
-add_action( 'wp_dashboard_setup', 'wsu_remove_dashboard_widgets' );
-/**
- * Remove all of the dashboard widgets and panels when a user logs
- * in except for the Right Now area.
- */
-function wsu_remove_dashboard_widgets() {
-	remove_meta_box( 'dashboard_recent_comments', 'dashboard', 'normal' );
-	remove_meta_box( 'dashboard_incoming_links' , 'dashboard', 'normal' );
-	remove_meta_box( 'tribe_dashboard_widget'   , 'dashboard', 'normal' );
-	remove_meta_box( 'dashboard_plugins'        , 'dashboard', 'normal' );
-	remove_meta_box( 'dashboard_primary'        , 'dashboard', 'side'   );
-	remove_meta_box( 'dashboard_secondary'      , 'dashboard', 'side'   );
-	remove_meta_box( 'dashboard_quick_press'    , 'dashboard', 'side'   );
-	remove_meta_box( 'dashboard_recent_drafts'  , 'dashboard', 'side'   );
+class WSUWP_WordPress_Dashboard {
 
-	remove_action( 'welcome_panel', 'wp_welcome_panel' );
-}
+	/**
+	 * Add our hooks.
+	 */
+	public function __construct() {
+		add_action( 'wp_dashboard_setup', array( $this, 'remove_dashboard_widgets' ) );
+		add_action( 'wp_network_dashboard_setup', array( $this, 'remove_network_dashboard_widgets' ) );
+	}
 
-add_action( 'wp_network_dashboard_setup', 'wsu_remove_network_dashboard_widgets' );
-function wsu_remove_network_dashboard_widgets() {
-	remove_meta_box( 'network_dashboard_right_now', 'dashboard-network', 'normal' );
-	remove_meta_box( 'dashboard_plugins'          , 'dashboard-network', 'normal' );
-	remove_meta_box( 'dashboard_primary'          , 'dashboard-network', 'side'   );
-	remove_meta_box( 'dashboard_secondary'        , 'dashboard-network', 'side'   );
+	/**
+	 * Remove all of the dashboard widgets and panels when a user logs
+	 * in except for the Right Now area.
+	 */
+	public function remove_dashboard_widgets() {
+		remove_meta_box( 'dashboard_recent_comments', 'dashboard', 'normal' );
+		remove_meta_box( 'dashboard_incoming_links' , 'dashboard', 'normal' );
+		remove_meta_box( 'tribe_dashboard_widget'   , 'dashboard', 'normal' );
+		remove_meta_box( 'dashboard_plugins'        , 'dashboard', 'normal' );
+		remove_meta_box( 'dashboard_primary'        , 'dashboard', 'side'   );
+		remove_meta_box( 'dashboard_secondary'      , 'dashboard', 'side'   );
+		remove_meta_box( 'dashboard_quick_press'    , 'dashboard', 'side'   );
+		remove_meta_box( 'dashboard_recent_drafts'  , 'dashboard', 'side'   );
+
+		remove_action( 'welcome_panel', 'wp_welcome_panel' );
+	}
+
+	/**
+	 * Remove all default widgets from the network dashboard.
+	 */
+	public function remove_network_dashboard_widgets() {
+		remove_meta_box( 'network_dashboard_right_now', 'dashboard-network', 'normal' );
+		remove_meta_box( 'dashboard_plugins'          , 'dashboard-network', 'normal' );
+		remove_meta_box( 'dashboard_primary'          , 'dashboard-network', 'side'   );
+		remove_meta_box( 'dashboard_secondary'        , 'dashboard-network', 'side'   );
+	}
 }
+$wsuwp_wordpress_dashboard = new WSUWP_WordPress_Dashboard();
