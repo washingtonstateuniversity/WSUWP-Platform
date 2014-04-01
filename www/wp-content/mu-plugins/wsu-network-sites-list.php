69ac578208802aff08e62e749eb801e472f3f1ba
jeremyfelt <jeremy.felt@gmail.com>
2014-03-31 17:07:52 -0700

    Add customizations for the MS Sites List Table
    Begins customizing the sites list table to provide more useful
    information for WSU network administrators.
    
    This gets us most of the way there. Will need to pick back up with
    some cleanup and documentation.

---
+++ www/wp-content/mu-plugins/wsu-network-sites-list.php
@@ -0,0 +1,126 @@
+<?php
+
+class WSU_Network_Sites_List {
+	public function __construct() {
+		add_filter( 'wpmu_blogs_columns', array( $this, 'site_columns' ) );
+		add_action( 'manage_sites_custom_column', array( $this, 'manage_sites_custom_column' ), 10, 2 );
+	}
+
+	public function site_columns( $site_columns ) {
+		$site_columns['site_name'] = 'Site Name';
+		$site_columns['site_created'] = 'Created';
+
+		unset( $site_columns['blogname'] );
+		unset( $site_columns['lastupdated'] );
+		unset( $site_columns['registered'] );
+		unset( $site_columns['users'] );
+
+		$site_columns['users'] = 'Users';
+		return $site_columns;
+	}
+
+	public function manage_sites_custom_column( $column, $site_id ) {
+		if ( 'site_name' === $column ) {
+			$this->display_site_name_row( $site_id );
+		} elseif ( 'site_created' === $column ) {
+			$this->display_site_created( $site_id );
+		}
+	}
+
+	private function display_site_name_row( $site_id ) {
+		$site_name = esc_html( get_blog_option( $site_id, 'blogname' ) );
+		?>
+		<a href="<?php echo esc_url( network_admin_url( 'site-info.php?id=' . absint( $site_id ) ) ); ?>" class="edit"><?php echo $site_name; ?></a>
+		<?php
+
+		// Preordered.
+		$actions = array(
+			'edit' => '', 'backend' => '',
+			'activate' => '', 'deactivate' => '',
+			'archive' => '', 'unarchive' => '',
+			'spam' => '', 'unspam' => '',
+			'delete' => '',
+			'visit' => '',
+		);
+
+		$actions['edit']	= '<span class="edit"><a href="' . esc_url( network_admin_url( 'site-info.php?id=' . $site_id ) ) . '">' . __( 'Edit' ) . '</a></span>';
+		$actions['backend']	= "<span class='backend'><a href='" . esc_url( get_admin_url( $site_id ) ) . "' class='edit'>" . __( 'Dashboard' ) . '</a></span>';
+		if ( get_current_site()->blog_id != $site_id ) {
+			if ( get_blog_status( $site_id, 'deleted' ) == '1' )
+				$actions['activate']	= '<span class="activate"><a href="' . esc_url( wp_nonce_url( network_admin_url( 'sites.php?action=confirm&amp;action2=activateblog&amp;id=' . $site_id . '&amp;msg=' . urlencode( sprintf( __( 'You are about to activate the site %s' ), $site_name ) ) ), 'confirm' ) ) . '">' . __( 'Activate' ) . '</a></span>';
+			else
+				$actions['deactivate']	= '<span class="activate"><a href="' . esc_url( wp_nonce_url( network_admin_url( 'sites.php?action=confirm&amp;action2=deactivateblog&amp;id=' . $site_id . '&amp;msg=' . urlencode( sprintf( __( 'You are about to deactivate the site %s' ), $site_name ) ) ), 'confirm') ) . '">' . __( 'Deactivate' ) . '</a></span>';
+
+			if ( get_blog_status( $site_id, 'archived' ) == '1' )
+				$actions['unarchive']	= '<span class="archive"><a href="' . esc_url( wp_nonce_url( network_admin_url( 'sites.php?action=confirm&amp;action2=unarchiveblog&amp;id=' . $site_id . '&amp;msg=' . urlencode( sprintf( __( 'You are about to unarchive the site %s.' ), $site_name ) ) ), 'confirm') ) . '">' . __( 'Unarchive' ) . '</a></span>';
+			else
+				$actions['archive']	= '<span class="archive"><a href="' . esc_url( wp_nonce_url( network_admin_url( 'sites.php?action=confirm&amp;action2=archiveblog&amp;id=' . $site_id . '&amp;msg=' . urlencode( sprintf( __( 'You are about to archive the site %s.' ), $site_name ) ) ), 'confirm') ) . '">' . _x( 'Archive', 'verb; site' ) . '</a></span>';
+
+			if ( get_blog_status( $site_id, 'spam' ) == '1' )
+				$actions['unspam']	= '<span class="spam"><a href="' . esc_url( wp_nonce_url( network_admin_url( 'sites.php?action=confirm&amp;action2=unspamblog&amp;id=' . $site_id . '&amp;msg=' . urlencode( sprintf( __( 'You are about to unspam the site %s.' ), $site_name ) ) ), 'confirm') ) . '">' . _x( 'Not Spam', 'site' ) . '</a></span>';
+			else
+				$actions['spam']	= '<span class="spam"><a href="' . esc_url( wp_nonce_url( network_admin_url( 'sites.php?action=confirm&amp;action2=spamblog&amp;id=' . $site_id . '&amp;msg=' . urlencode( sprintf( __( 'You are about to mark the site %s as spam.' ), $site_name ) ) ), 'confirm') ) . '">' . _x( 'Spam', 'site' ) . '</a></span>';
+
+			if ( current_user_can( 'delete_site', $site_id ) )
+				$actions['delete']	= '<span class="delete"><a href="' . esc_url( wp_nonce_url( network_admin_url( 'sites.php?action=confirm&amp;action2=deleteblog&amp;id=' . $site_id . '&amp;msg=' . urlencode( sprintf( __( 'You are about to delete the site %s.' ), $site_name ) ) ), 'confirm') ) . '">' . __( 'Delete' ) . '</a></span>';
+		}
+
+		$actions['visit']	= "<span class='view'><a href='" . esc_url( get_home_url( $site_id, '/' ) ) . "' rel='permalink'>" . __( 'Visit' ) . '</a></span>';
+
+		/**
+		 * Filter the action links displayed for each site in the Sites list table.
+		 *
+		 * The 'Edit', 'Dashboard', 'Delete', and 'Visit' links are displayed by
+		 * default for each site. The site's status determines whether to show the
+		 * 'Activate' or 'Deactivate' link, 'Unarchive' or 'Archive' links, and
+		 * 'Not Spam' or 'Spam' link for each site.
+		 *
+		 * @since 3.1.0
+		 *
+		 * @param array  $actions  An array of action links to be displayed.
+		 * @param int    $blog_id  The site ID.
+		 * @param string $blogname Site path, formatted depending on whether it is a sub-domain
+		 *                         or subdirectory multisite install.
+		 */
+		$actions = apply_filters( 'manage_sites_action_links', array_filter( $actions ), $site_id, $site_name );
+		echo $this->row_actions( $actions );
+	}
+
+	private function display_site_created( $site_id ) {
+		switch_to_blog( $site_id );
+		$site_details = get_blog_details();
+		restore_current_blog();
+
+		if ( isset( $site_details->registered ) ) {
+			echo $site_details->registered;
+		}
+	}
+
+	/**
+	 * Row actions copied directly from class-wp-list-table so that we can
+	 * use it with our custom column.
+	 *
+	 * @param array $actions
+	 * @param bool  $always_visible
+	 *
+	 * @return string HTML output for row actions.
+	 */
+	function row_actions( $actions, $always_visible = false ) {
+		$action_count = count( $actions );
+		$i = 0;
+
+		if ( !$action_count )
+			return '';
+
+		$out = '<div class="' . ( $always_visible ? 'row-actions visible' : 'row-actions' ) . '">';
+		foreach ( $actions as $action => $link ) {
+			++$i;
+			( $i == $action_count ) ? $sep = '' : $sep = ' | ';
+			$out .= "<span class='$action'>$link$sep</span>";
+		}
+		$out .= '</div>';
+
+		return $out;
+	}
+}
+new WSU_Network_Sites_List();
