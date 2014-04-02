e73ed918e3c11b3d13c2f8905f6b7f6b158f9c61
jeremyfelt <jeremy.felt@gmail.com>
2014-04-02 13:47:34 -0700

    Add initial support for a Deployment custom post type
    This post type will be used to track deploy hooks that are setup
    with GitHub. In the future, we can hopefully use these to connect
    to other VCS as well.

---
+++ www/wp-content/mu-plugins/wsu-deployment.php
@@ -0,0 +1,64 @@
+<?php
+/*
+Plugin Name: WSU Deployment
+Plugin URI: http://web.wsu.edu
+Description: Receive deploy requests and act accordingly.
+Author: washingtonstateuniversity, jeremyfelt
+Version: 0.1
+*/
+
+class WSU_Deployment {
+
+	/**
+	 * @var string Slug to track the deployment post type.
+	 */
+	var $post_type_slug = 'wsuwp_deployment';
+
+	/**
+	 * Add hooks.
+	 */
+	public function __construct() {
+		add_action( 'init', array( $this, 'register_post_type' ) );
+	}
+
+	/**
+	 * Register the deployment post type to track the deployments that
+	 * have been created.
+	 */
+	public function register_post_type() {
+		// Only enable this on the network's primary site.
+		if ( ! is_main_site() || ! is_main_network() ) {
+			return;
+		}
+
+		$labels = array(
+			'name' => 'Deployments',
+			'singular_name' => 'Deployment',
+			'add_new' => 'Add New',
+			'add_new_item' => 'Add New Deployment',
+			'edit_item' => 'Edit Deployment',
+			'new_item' => 'New Deployment',
+			'all_items' => 'All Deployments',
+			'view_item' => 'View Deployments',
+			'search_items' => 'Search Deployments',
+			'not_found' => 'No deployments found',
+			'not_found_in_trash' => 'No deployments found in Trash',
+			'menu_name' => 'Deployments',
+		);
+
+		$args = array(
+			'labels'             => $labels,
+			'public'             => true,
+			'publicly_queryable' => true,
+			'show_ui'            => true,
+			'show_in_menu'       => true,
+			'query_var'          => true,
+			'rewrite'            => array( 'slug' => 'deployment' ),
+			'has_archive'        => false,
+			'hierarchical'       => false,
+			'supports'           => array( 'title', ),
+		);
+		register_post_type( $this->post_type_slug, $args );
+	}
+}
+new WSU_Deployment();
