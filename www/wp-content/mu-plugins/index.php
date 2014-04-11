50388153a2af6a3da52c2734ae8efb7140f8692e
jeremyfelt <jeremy.felt@gmail.com>
2014-04-11 09:10:32 -0700

    Bump WordPress and global wsuwp version for push to production

--- www/wp-content/mu-plugins/index.php
+++ www/wp-content/mu-plugins/index.php
@@ -9,7 +9,7 @@
  * Network: true
  */
 
-$wsuwp_global_version = '0.5.9';
-$wsuwp_wp_changeset = '28058';
+$wsuwp_global_version = '0.5.10';
+$wsuwp_wp_changeset = '28072';
 
 include dirname( __FILE__ ) . '/wsu-co-authors-plus-skin/wsu-co-authors-plus-skin.php';
