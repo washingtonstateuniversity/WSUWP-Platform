bed167b0d6f5c2c047a885144c1109a3e599b8c7
jeremyfelt <jeremy.felt@gmail.com>
2014-04-18 16:00:47 -0700

    Display a drop down of all networks with the current selected

--- www/wp-content/mu-plugins/wsu-network-site-info.php
+++ www/wp-content/mu-plugins/wsu-network-site-info.php
@@ -19,13 +19,24 @@ class WSU_Network_Site_Info {
 	 * Output the initial options for moving the site.
 	 */
 	public function display_move_site() {
-		if ( 'site-info-network' !== get_current_screen()->id ) {
+		global $id;
+
+		if ( 'site-info-network' !== get_current_screen()->id || ! is_super_admin() ) {
 			return;
 		}
+
+		$site_network_id = get_blog_details( $id )->site_id;
 		?>
 		<table style="display:none;"><tr id="wsu-move-site" class="form-field form-required">
 			<th scope="row"><?php _e( 'Network' ) ?></th>
-			<td><select name="wsu_network"><option value="">--- Select Network ---</option></select></td>
+			<td><select name="wsu_network">
+		<?php
+		$networks = wsuwp_get_networks();
+		foreach( $networks as $network ) {
+			echo '<option value="' . $network->id . '" ' . selected( $site_network_id, $network->id, false ) . '>' . $network->domain . $network->path . '</option>';
+		}
+		?>
+				</select></td>
 		</tr></table>
 		<?php
 	}
