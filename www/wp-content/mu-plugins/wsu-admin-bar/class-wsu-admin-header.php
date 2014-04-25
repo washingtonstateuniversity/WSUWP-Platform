92664822ef001615e94a7e000d6ea166f48050d8
jeremyfelt <jeremy.felt@gmail.com>
2014-04-25 12:04:55 -0700

    Only show My Networks and My Sites in admin menu when appropriate
    This modifies the previous logic a couple ways:
    
    * If a user has been added to a network, but to 0 sites, do not show
      a menu that may indicate otherwise. This removes the My Sites and
      My Networks display.
    * If a user is not a super admin, but is a member of more than one
      site, DO show the my networks menu. We'll get better at how that
      looks in the near future.

--- www/wp-content/mu-plugins/wsu-admin-bar/class-wsu-admin-header.php
+++ www/wp-content/mu-plugins/wsu-admin-bar/class-wsu-admin-header.php
@@ -55,17 +55,17 @@ class WSU_Admin_Header {
 		$user_sites = wsuwp_get_user_sites( get_current_user_id() );
 
 		/**
-		 * The user is not a super admin and they only belong to one network. At this point
-		 * it's most likely that we should not give them access to a networks menu. If they
-		 * only belong to one site as well, then we should remove the My Sites menu item to
-		 * avoid confusion.
+		 * If the user is a member of only one site, we can assume they are also a member of
+		 * only one network. If this is the case *and* they are not a super admin of this
+		 * network, which is properly determined during the page load, then we remove the
+		 * My Sites menu entirely and return without creating a My Networks menu.
 		 *
-		 * @todo - determine real capabilities here rather than the blanket super admin role
+		 * This also catches a case in which a user has been added as a member of a network,
+		 * but does not have access to any individual site yet. At that point, a blank
+		 * admin bar will be displayed.
 		 */
-		if ( ! is_super_admin() && 1 === count( $user_sites ) ) {
+		if ( ! is_super_admin() && 1 >= count( $user_sites ) ) {
 			$wp_admin_bar->remove_menu( 'my-sites' );
-			return;
-		} elseif ( ! is_super_admin() ) {
 			return;
 		}
 
