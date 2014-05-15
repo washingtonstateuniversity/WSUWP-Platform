bc80bdec3e9c31da47a9dbab93968daa4fed65c7
jeremyfelt <jeremy.felt@gmail.com>
2014-05-15 13:27:55 -0700

    Add initial Gruntfile.js for packaging files for a build

---
+++ Gruntfile.js
@@ -0,0 +1,44 @@
+module.exports = function(grunt) {
+	// Project configuration
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		copy: {
+			main: {
+				files: [
+					{
+						expand: true,
+						src: 'wordpress/**',
+						dest: 'build/',
+						cwd: 'www'
+					},
+					{
+						expand: true,
+						src: ['wp-content/mu-plugins/**','!/www/wp-content/mu-plugins/local-index.php'],
+						dest: 'build/',
+						cwd: 'www'
+					},
+					{
+						expand: true,
+						src: 'wp-content/plugins/index.php',
+						dest: 'build/',
+						cwd: 'www'
+					},
+					{
+						expand: true,
+						src: [
+							'wp-content/themes/twentytwelve/**',
+							'wp-content/themes/twentythirteen/**',
+							'wp-content/themes/twentyfourteen/**'
+						],
+						dest: 'build/',
+						cwd: 'www'
+					}
+				]
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-copy');
+
+	grunt.registerTask('default', ['copy']);
+};
