17685cda0fbc6f8af5394f3beafb873ce9f8933b
jeremyfelt <jeremy.felt@gmail.com>
2014-05-28 14:47:59 -0700

    Bump version to 0.6.15 for deploy

--- www/wp-content/mu-plugins/index.php
+++ www/wp-content/mu-plugins/index.php
@@ -9,7 +9,7 @@
  * Network: true
  */
 
-$wsuwp_global_version = '0.6.14';
+$wsuwp_global_version = '0.6.15';
 $wsuwp_wp_changeset = '28354';
 
 include dirname( __FILE__ ) . '/wsu-co-authors-plus-skin/wsu-co-authors-plus-skin.php';
