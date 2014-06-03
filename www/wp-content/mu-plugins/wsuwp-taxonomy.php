5efde38a03600399a10591e5e0b9360271c0b1f0
jeremyfelt <jeremy.felt@gmail.com>
2014-06-03 10:49:34 -0700

    Enable categories and tags for pages
    Fixes #126

---
+++ www/wp-content/mu-plugins/wsuwp-taxonomy.php
@@ -0,0 +1,32 @@
+<?php
+/*
+ * Plugin Name: WSUWP Taxonomy Management
+ * Plugin URI: http://web.wsu.edu
+ * Description: Connects dots on various taxonomy tasks.
+ * Author: washingtonstateuniversity, jeremyfelt
+ * Author URI: http://web.wsu.edu
+ * Version: 0.1
+ * Network: true
+ */
+
+/**
+ * Class WSUWP_Taxonomy
+ */
+class WSUWP_Taxonomy {
+
+	/**
+	 * Add our hooks.
+	 */
+	public function __construct() {
+		add_action( 'init', array( $this, 'add_taxonomies_to_pages' ) );
+	}
+
+	/**
+	 * Register built in taxonomies - Categories and Tags - to pages.
+	 */
+	public function add_taxonomies_to_pages() {
+		register_taxonomy_for_object_type( 'category', 'page' );
+		register_taxonomy_for_object_type( 'post_tag', 'page' );
+	}
+}
+new WSUWP_Taxonomy();
