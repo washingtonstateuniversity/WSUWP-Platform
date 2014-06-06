17c8d0e3b032c51bf8d08b188a23260c0287cc48
jeremyfelt <jeremy.felt@gmail.com>
2014-06-06 12:58:40 -0700

    Remove filter on various URIs to avoid CORS issues
    We were trying to use the same domain - wp.wsu.edu on everything,
    rather than having a content URL for each individual site. That's
    pretty ridiculous and could cause other, unknown issues down the
    road. Right now it's causing an issue with a CORS request for some
    font awesome icons.

--- www/wp-content/mu-plugins/wsu-core-filters.php
+++ www/wp-content/mu-plugins/wsu-core-filters.php
@@ -6,25 +6,3 @@ Description: Various filters used to modify core data.
 Author: washingtonstateuniversity, jeremyfelt
 Version: 0.1
 */
-
-add_filter( 'theme_root_uri', 'wsuwp_modify_root_uri' );
-add_filter( 'content_url', 'wsuwp_modify_root_uri' );
-add_filter( 'wp_get_attachment_url', 'wsuwp_modify_root_uri' );
-/**
- * Load theme resources from the home URL rather than the network
- * URL. This can help group DNS requests a bit and avoid unnecessary
- * lookups at the primary domain.
- *
- * @param string $theme_root_uri Current root URI for the theme.
- *
- * @return string Modified root URI for the theme.
- */
-function wsuwp_modify_root_uri( $theme_root_uri ) {
-	$theme_root = parse_url( $theme_root_uri );
-	$home_root = parse_url( get_home_url() );
-
-	if ( isset( $theme_root['host'] ) && isset( $home_root['host'] ) ) {
-		$theme_root_uri = str_replace( '/' . $theme_root['host'] . '/', '/' . $home_root['host'] . '/', $theme_root_uri );
-	}
-	return $theme_root_uri;
-}
