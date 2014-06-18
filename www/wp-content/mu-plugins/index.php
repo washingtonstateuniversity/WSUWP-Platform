e1e513071e41ccd00360ad3f75cd32fbdb827326
jeremyfelt <jeremy.felt@gmail.com>
2014-06-18 14:05:25 -0700

    Bump version to 0.8.2

--- www/wp-content/mu-plugins/index.php
+++ www/wp-content/mu-plugins/index.php
@@ -9,7 +9,7 @@
  * Network: true
  */
 
-$wsuwp_global_version = '0.8.1';
+$wsuwp_global_version = '0.8.2';
 $wsuwp_wp_changeset = '28354';
 
 add_filter( 'spine_enable_builder_module', '__return_true' );
