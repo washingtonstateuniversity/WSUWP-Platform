14be95a81743d6096859a4e338ab78a963e89a21
jeremyfelt <jeremy.felt@gmail.com>
2014-07-17 12:07:34 -0700

    Bump version to 1.0.4

--- www/wp-content/mu-plugins/index.php
+++ www/wp-content/mu-plugins/index.php
@@ -5,11 +5,11 @@
  * Description: Controls WSUWP Platform global version
  * Author: washingtonstateuniversity, jeremyfelt
  * Author URI: http://web.wsu.edu
- * Version: 1.0.3
+ * Version: 1.0.4
  * Network: true
  */
 
-$wsuwp_global_version = '1.0.3';
+$wsuwp_global_version = '1.0.4';
 $wsuwp_wp_changeset = '28354';
 
 add_filter( 'spine_enable_builder_module', '__return_true' );
