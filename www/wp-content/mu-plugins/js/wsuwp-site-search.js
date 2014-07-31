4e125af2ef15bcadb524e94d6a9ffdf0d6b766f7
jeremyfelt <jeremy.felt@gmail.com>
2014-07-31 11:30:30 -0700

    Add functionality to search through long site lists in admin header

---
+++ www/wp-content/mu-plugins/js/wsuwp-site-search.js
@@ -0,0 +1,13 @@
+/** Adapted from Kailey Lampert's My Sites Search - https://github.com/trepmal/my-sites-search/ */
+(function($){
+	jQuery(document).ready( function($) {
+		$('.ms-sites-search.hide-if-no-js').show();
+		$('.ms-sites-search input').keyup( function( ) {
+			var searchValRegex = new RegExp( $(this).val(), 'i');
+			var current_menu = $(this ).closest('ul' ).attr('id');
+			$( '#' + current_menu + ' > li.menupop').hide().filter(function() {
+				return searchValRegex.test( $(this).find('> a').text() );
+			}).show();
+		});
+	});
+}(jQuery));
