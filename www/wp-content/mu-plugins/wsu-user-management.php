7afc2020d44d69ec49517531661b5c7de93c298d
jeremyfelt <jeremy.felt@gmail.com>
2016-02-03 11:24:33 -0800

    Clarify instructions on new network user screen

--- www/wp-content/mu-plugins/wsu-user-management.php
+++ www/wp-content/mu-plugins/wsu-user-management.php
@@ -51,7 +51,7 @@ class WSU_User_Management {
 				(function($){
 					$('.form-table' ).find('td').last().html('<p class="description" style="max-width:640px;">Creating a new user on ' +
 						'this page does not send any notification email. Please communicate with the new user as appropriate. If you ' +
-						'would like a notification to be generated automatically, create the user at an individual site level.</p>');
+						'would like a notification to be generated automatically, create the user in the admin of an individual site.</p>');
 				}(jQuery));
 			</script><?php
 		}
