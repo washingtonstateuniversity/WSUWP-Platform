6250e515a482dfa0ebbead1dcc88badd507abd37
Jeremy Felt <jeremy.felt@gmail.com>
2016-04-25 15:08:54 -0700

    [PR] Account for port numbers in sunrise routing (#338)
    * Strip port numbers from HTTP_HOST data
    
    If a port number is attached here, it will fail our other lookups
    and result in a redirect to wsu.edu. Not many, if any valid requests
    come through with a port number attached, but we should still be
    defensive.
    
    * Redirect invalid sites to the default wp.wsu.edu site with HTTPS
    
    * Use yoda conditionals and strict type checking

--- www/wp-content/sunrise.php
+++ www/wp-content/sunrise.php
@@ -50,9 +50,18 @@ if ( defined( 'COOKIE_DOMAIN' ) ) {
 	die( 'The constant "COOKIE_DOMAIN" is defined (probably in wp-config.php). Please remove or comment out that define() line.' );
 }
 
-//Capture the domain and path from the current request
-$requested_domain    = $_SERVER['HTTP_HOST'];
+// Capture the domain and path from the current request
+$requested_domain = strtolower( $_SERVER['HTTP_HOST'] );
 $requested_uri       = trim( $_SERVER['REQUEST_URI'], '/' );
+
+// Strip any port numbers from the HTTP_HOST data.
+if ( ':80' === substr( $requested_domain, -3 ) ) {
+	$requested_domain = substr( $requested_domain, 0, -3 );
+	$_SERVER['HTTP_HOST'] = substr( $_SERVER['HTTP_HOST'], 0, -3 );
+} elseif ( ':443' === substr( $requested_domain, -4 ) ) {
+	$requested_domain = substr( $requested_domain, 0, -4 );
+	$_SERVER['HTTP_HOST'] = substr( $_SERVER['HTTP_HOST'], 0, -4 );
+}
 
 // We currently support one subdirectory deep, and therefore only look at the first path level
 $requested_uri_parts = explode( '/', $requested_uri );
@@ -188,7 +197,7 @@ if( $current_blog ) {
 		header( "Location: http://" . $redirect_domain, true, 301 );
 	} else {
 		error_log( 'Sunrise: Redirecting invalid domain to default site - ' . $requested_domain );
-		header( "Location: http://wp.wsu.edu/", true, 301 );
+		header( "Location: https://wp.wsu.edu/", true, 301 );
 	}
 
 	die();
