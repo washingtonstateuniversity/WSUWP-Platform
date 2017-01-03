a6a326012a089e042273f67dfcbe3a4d305ea3dc
Jeremy Felt <jeremy.felt@gmail.com>
2017-01-03 11:12:52 -0800

    Bump version 1.5.5

--- www/wp-content/mu-plugins/index.php
+++ www/wp-content/mu-plugins/index.php
@@ -5,12 +5,12 @@
  * Description: Controls WSUWP Platform global version
  * Author: washingtonstateuniversity, jeremyfelt
  * Author URI: https://web.wsu.edu/
- * Version: 1.5.4
+ * Version: 1.5.5
  * Network: true
  */
 
-$wsuwp_global_version = '1.5.4';
-$wsuwp_wp_changeset = '39525';
+$wsuwp_global_version = '1.5.5';
+$wsuwp_wp_changeset = '39525-m1';
 
 /**
  * Returns the current deployment version of WSUWP Platform
