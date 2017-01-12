washingtonstateuniversity/WSUWP-Platform

commit 9c5c36453ddb4a5e61f44a58e38bded1ff6caab7

Adjust queries throwing errors in PHPCS to be more prepared

www/wp-content/mu-plugins/wsu-core-functions.php
@@ -30,7 +30,8 @@ function wsuwp_get_user_networks( $user_id = null ) {
 	}
 
 	$user_id = absint( $user_id );
-	$network_keys = $wpdb->get_col( "SELECT meta_key FROM $wpdb->usermeta WHERE user_id = $user_id AND meta_key LIKE 'wsuwp_network_%_capabilities'" );
+	$sql = $wpdb->prepare( "SELECT meta_key FROM $wpdb->usermeta WHERE user_id = %d AND meta_key LIKE 'wsuwp_network_%_capabilities'", $user_id );
+	$network_keys = $wpdb->get_col( $sql ); // WPCS: unprepared SQL OK.
 
 	$user_network_ids = array();
 
@@ -345,7 +346,7 @@ We hope you enjoy your new site. Thanks!
 
 		$insert .= $wpdb->prepare( '( %d, %s, %s )', $network_id, $meta_key, $meta_value );
 	}
-	$wpdb->query( "INSERT INTO $wpdb->sitemeta ( site_id, meta_key, meta_value ) VALUES " . $insert );
+	$wpdb->query( "INSERT INTO $wpdb->sitemeta ( site_id, meta_key, meta_value ) VALUES " . $insert ); // WPCS: unprepared SQL OK.
 }
 
 /**

www/wp-content/mu-plugins/wsu-network-admin.php
@@ -682,7 +682,7 @@ class WSU_Network_Admin {
 		$network = get_network( $network_id );
 
 		$query = $wpdb->prepare( "SELECT * FROM {$wpdb->sitemeta} WHERE site_id = %d", $network_id );
-		$network_data = $wpdb->get_results( $query, ARRAY_A );
+		$network_data = $wpdb->get_results( $query, ARRAY_A ); // WPCS: unprepared SQL OK.
 
 		$network_display_fields = array(
 			'blog_count',
@@ -895,7 +895,7 @@ class WSU_Network_Admin {
 		}
 
 		$meta_key = 'wsuwp_network_' . $wpdb->siteid . '_capabilities';
-		$count = $wpdb->get_var( "SELECT COUNT(user_id) FROM $wpdb->usermeta WHERE meta_key = '$meta_key'" );
+		$count = $wpdb->get_var( $wpdb->prepare( "SELECT COUNT(user_id) FROM $wpdb->usermeta WHERE meta_key = %s", $meta_key ) );
 		return $count;
 	}
 }

commit 75047771cbed07b74dd174f0d1c3f4bc419fd2f8

Correct the query used to retrieve a user's networks
This was broken in 9c5c36453ddb4a5e61f44a58e38bded1ff6caab7 and
would result in the return of all networks for all users. This
caused the "My Networks" menu to appear for everyone and likely
caused with some slowdown issues.

www/wp-content/mu-plugins/wsu-core-functions.php
@@ -30,7 +30,10 @@ function wsuwp_get_user_networks( $user_id = null ) {
 	}
 
 	$user_id = absint( $user_id );
-	$sql = $wpdb->prepare( "SELECT meta_key FROM $wpdb->usermeta WHERE user_id = %d AND meta_key LIKE 'wsuwp_network_%_capabilities'", $user_id );
+
+	$sql = $wpdb->prepare( "SELECT meta_key FROM $wpdb->usermeta WHERE user_id = %d", $user_id );
+	$sql .= " AND meta_key LIKE 'wsuwp_network_%_capabilities'";
+
 	$network_keys = $wpdb->get_col( $sql ); // WPCS: unprepared SQL OK.
 
 	$user_network_ids = array();