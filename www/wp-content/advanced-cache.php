2afec699be980dc3a127a1041068b9214b94f583
Jeremy Felt <jeremy.felt@gmail.com>
2017-11-13 11:53:08 -0800

    Really never page cache REST API content

--- www/wp-content/advanced-cache.php
+++ www/wp-content/advanced-cache.php
@@ -332,7 +332,7 @@ if ( preg_match( '/documents\/([0-9]{4})\/([0-9]{1,2})\/([^.]+)\.[A-Za-z0-9]{3,4
 
 // Never batcache REST API content
 if ( preg_match( '/wp-json\//', $_SERVER['REQUEST_URI'] ) ) {
-	$batcache->url_key = md5( 'rest-api' );
+	return;
 }
 
 // Never batcache WP javascript generators
